a719069504a61843d5e6b5916c03ce9ed0f7edce
Josh Scott <joshscott82@gmail.com>
2022-12-12 17:37:31 -0500

    updates

--- scott-web/src/app/app-routing.module.ts
+++ scott-web/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { E404Component } from './e404/e404.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "/home", pathMatch: "full"},
+
+
+  {path: "home", component: HomeComponent},
+  {path: "about", component: AboutComponent},
+
+
+
+  {path: "**", component: E404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- scott-web/src/app/app.module.ts
+++ scott-web/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { E404Component } from './e404/e404.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    E404Component,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
